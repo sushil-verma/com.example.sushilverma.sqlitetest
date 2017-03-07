ada13c59c455d27ca7005f8a05e1b2bda356b9c7
sushil.verma <sushilverma1985@gmail.com>
2017-03-07 10:21:08 +0530

    Sqlite

---
+++ Contact.java
@@ -0,0 +1,59 @@
+package com.example.sushilverma.sqlitetest;
+
+/**
+ * Created by sushil.verma on 2/8/2017.
+ */
+
+public class Contact {
+
+    //private variables
+    private   int _id;
+    private String _name;
+    private String _phone_number;
+
+    // Empty constructor
+    public Contact(){
+
+    }
+    // constructor
+    public Contact(int id, String name, String _phone_number){
+        this._id = id;
+        this._name = name;
+        this._phone_number = _phone_number;
+    }
+
+    // constructor
+    public Contact(String name, String _phone_number){
+        this._name = name;
+        this._phone_number = _phone_number;
+    }
+    // getting ID
+    public int getID(){
+        return this._id;
+    }
+
+    // setting id
+    public void setID(int id){
+        this._id = id;
+    }
+
+    // getting name
+    public String getName(){
+        return this._name;
+    }
+
+    // setting name
+    public void setName(String name){
+        this._name = name;
+    }
+
+    // getting phone number
+    public String getPhoneNumber(){
+        return this._phone_number;
+    }
+
+    // setting phone number
+    public void setPhoneNumber(String phone_number){
+        this._phone_number = phone_number;
+    }
+}
